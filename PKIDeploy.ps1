d0ea6af1649aca5ad394d9fd90cf1a523729d7f1
Melissa Januszko <majst32@yahoo.com>
2017-02-28 10:47:10 -0500

    Initial commit

---
+++ PKIDeploy.ps1
@@ -0,0 +1,39 @@
+﻿Configuration PKIDeploy {
+
+        Import-DSCresource -ModuleName PSDesiredStateConfiguration,@{ModuleName="xADCSDeployment";ModuleVersion="1.1.0.0"}
+
+    Node $AllNodes.Where{$_.Role -eq "ADCSRoot"}.NodeName {
+
+        $ADCSRoot = $ConfigurationData.ADCSRoot
+
+        $Secure = ConvertTo-SecureString -String "$($Node.Password)" -AsPlainText -Force 
+        $Credential = New-Object -typename Pscredential -ArgumentList Administrator, $secure 
+
+        foreach ($Feature in $ADCSRoot.Features) {
+
+            WindowsFeature $Feature {
+                Name = $Feature
+                Ensure = 'Present'
+                }
+                
+        }
+                 
+            xAdcsCertificationAuthority ADCSConfig {
+                CAType = $ADCSRoot.CAType
+                Credential = $Credential
+                CryptoProviderName = $ADCSRoot.ADCSCryptoProviderName
+                HashAlgorithmName = $ADCSRoot.ADCSHashAlgorithmName
+                KeyLength = $ADCSRoot.ADCSKeyLength
+                CACommonName = $ADCSRoot.CACN
+                CADistinguishedNameSuffix = $ADCSRoot.CADNSuffix
+                DatabaseDirectory = $ADCSRoot.CADatabasePath
+                LogDirectory = $ADCSRoot.CALogPath
+                ValidityPeriod = $ADCSRoot.ADCSValidityPeriod
+                ValidityPeriodUnits = $ADCSRoot.ADCSValidityPeriodUnits
+                Ensure = 'Present'
+                DependsOn = '[WindowsFeature]ADCS-Cert-Authority'
+                }
+    }
+}
+
+PKIDeploy -ConfigurationData .\PKIDeploy.psd1 
---
+++ PKIDeploy.psd1
@@ -0,0 +1,31 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeName = '*'
+            PSDscAllowPlainTextPassword = $true
+           }
+      
+     @{
+            NodeName = 'TgtPull'
+            Role = 'ADCSRoot'
+            Password = 'P@ssw0rd'
+        }
+    )
+    ADCSRoot = @{
+            # ADCS Certificate Services information  for offline root
+            Features = @('ADCS-Cert-Authority';'RSAT-ADCS-Mgmt')
+            CAType = 'StandaloneRootCA'
+            CACN = 'CompanyRoot'
+            CADNSuffix = 'C=US,L=Philadelphia,S=Pennsylvania,O=Company'
+            CADatabasePath = 'C:\windows\system32\CertLog'
+            CALogPath = 'C:\CA_Logs'
+            ADCSCryptoProviderName = 'RSA#Microsoft Software Key Storage Provider'
+            ADCSHashAlgorithmName = 'SHA256'
+            ADCSKeyLength = '2048'
+            ADCSValidityPeriod = 'Years'
+            ADCSValidityPeriodUnits = '2'
+            }
+
+    #ADCSSub = @{
+            # ADCS Certificate Services info for Enterprise Subordinate
+ }
